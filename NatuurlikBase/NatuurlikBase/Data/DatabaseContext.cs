8ca6e1a30424faf09f4a74620fc7e02e64e3a77b
ThenjiweN <thenjiwentsonda@gmail.com>
2022-04-13 21:03:49 +0200

    Added Project Env

---
+++ NatuurlikBase/NatuurlikBase/Data/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NatuurlikBase.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) :
+            base(options)
+        {
+
+        }
+    }
+}
