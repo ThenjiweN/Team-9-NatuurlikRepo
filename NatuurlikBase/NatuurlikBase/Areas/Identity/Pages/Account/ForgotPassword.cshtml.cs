94f653882caf29816bf5fbd122d2eb885ee91c0d
ChibsMimi <u17254061@tuks.gmail.com>
2022-05-01 21:53:54 +0200

    Added forgot password code and the product catalogue

--- NatuurlikBase/NatuurlikBase/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
+++ NatuurlikBase/NatuurlikBase/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
@@ -30,23 +30,11 @@ namespace NatuurlikBase.Areas.Identity.Pages.Account
             _emailSender = emailSender;
         }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         [BindProperty]
         public InputModel Input { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         public class InputModel
         {
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
             [Required]
             [EmailAddress]
             public string Email { get; set; }
@@ -59,18 +47,16 @@ namespace NatuurlikBase.Areas.Identity.Pages.Account
                 var user = await _userManager.FindByEmailAsync(Input.Email);
                 if (user == null || !(await _userManager.IsEmailConfirmedAsync(user)))
                 {
-                    // Don't reveal that the user does not exist or is not confirmed
                     return RedirectToPage("./ForgotPasswordConfirmation");
                 }
 
-                // For more information on how to enable account confirmation and password reset please
-                // visit https://go.microsoft.com/fwlink/?LinkID=532713
                 var code = await _userManager.GeneratePasswordResetTokenAsync(user);
                 code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                var resetEmail = Input.Email;
                 var callbackUrl = Url.Page(
                     "/Account/ResetPassword",
                     pageHandler: null,
-                    values: new { area = "Identity", code },
+                    values: new { area = "Identity", code, resetEmail = resetEmail },
                     protocol: Request.Scheme);
 
                 await _emailSender.SendEmailAsync(
