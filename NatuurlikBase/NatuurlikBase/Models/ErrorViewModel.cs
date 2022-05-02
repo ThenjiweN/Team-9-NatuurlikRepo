f505f41f91841f9a23da6a39262d891405587a48
ThenjiweN <thenjiwentsonda@gmail.com>
2022-05-02 21:37:34 +0200

    Removed BuggyBase

--- NatuurlikBase/NatuurlikBase/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NatuurlikBase.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
