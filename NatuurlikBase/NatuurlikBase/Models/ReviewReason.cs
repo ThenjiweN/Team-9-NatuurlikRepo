6d5dd1629e79a0f8322c898c434584c95863d803
Aphiwe <preciousshozi789@gmail.com>
2022-05-28 10:59:25 +0200

    Revert "Revert "Query Reason and Review Reason""
    This reverts commit 5e395d93aa365c64086b26c485a2b48d79eb7b21.

---
+++ NatuurlikBase/NatuurlikBase/Models/ReviewReason.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NatuurlikBase.Models
+{
+    public class ReviewReason
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Review Reason Name")]
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
+
